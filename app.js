c394e66caa119c2ecc9b4ff35aecb69c7c0b63c1
Nicole Ogawa <nogawa11@gmail.com>
2022-03-25 14:09:18 +0900

    added reset function

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelectorAll('.button-tip');
+const tip = document.querySelector('.tip');
 let tipPercent = '';
 const custom = document.querySelector('.button-custom');
 const bill = document.querySelector('.bill');
@@ -8,21 +9,37 @@ let numberOfPeople = '';
 let tipAmount = document.querySelector('.tip-amount');
 let total = document.querySelector('.total');
 const reset = document.querySelector('.button-reset');
+const errorBill = document.querySelector('.error-message-bill');
+const errorPeople = document.querySelector('.error-message-people');
 
 bill.addEventListener('input', (event) => {
   billAmount = event.target.value;
-  console.log(billAmount);
+  errorBill.classList.remove('display')
+  bill.classList.remove('error');
+  if (billAmount === '0') {
+    bill.classList.add('error');
+    errorBill.classList.add('display');
+  }
+  calculateTip();
 })
 
 number.addEventListener('input', (event) => {
   numberOfPeople = event.target.value;
-  console.log(numberOfPeople);
+  number.classList.remove('error')
+  errorPeople.classList.remove('display');
+  if (numberOfPeople === '0') {
+    number.classList.add('error')
+    errorPeople.classList.add('display');
+  }
   calculateTip();
 })
 
 custom.addEventListener('input', (event) => {
+  buttons.forEach((button) => {
+    button.classList.remove('active')
+  });
   tipPercent = event.target.value/100;
-  console.log(tipPercent);
+  calculateTip();
 })
 
 buttons.forEach((button) => {
@@ -32,12 +49,27 @@ buttons.forEach((button) => {
       button.classList.remove('active')
     });
     event.target.classList.add('active');
-    console.log(tipPercent);
+    custom.value = "";
+    calculateTip();
   })
 })
 
 const calculateTip = () => {
-  tipAmount.innerText = `$${(billAmount * tipPercent).toFixed(2)}`;
-  total.innerText = `$${(billAmount * tipPercent / numberOfPeople).toFixed(2)}`;
-  reset.classList.remove('disabled');
+  if ((numberOfPeople > 0) && (tipPercent > 0) && (billAmount > 0)) {
+    tipAmount.innerText = `$${(billAmount * tipPercent).toFixed(2)}`;
+    total.innerText = `$${(billAmount * tipPercent / numberOfPeople).toFixed(2)}`;
+    reset.classList.remove('disabled');
+  }
 }
+
+reset.addEventListener('click', (event) => {
+  tipAmount.innerText = '$0.00'
+  total.innerText = '$0.00'
+  event.target.classList.add('disabled');
+  buttons.forEach((button) => {
+    button.classList.remove('active')
+  });
+  bill.value = "";
+  number.value = "";
+  custom.value = "";
+})
