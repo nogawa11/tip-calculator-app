93a90fd47c72dbb249c65d65d99445ab2f8ee710
Nicole Ogawa <nogawa11@gmail.com>
2022-03-25 15:32:29 +0900

    refactored app.js

--- app.js
+++ app.js
@@ -1,75 +1,74 @@
-const buttons = document.querySelectorAll('.button-tip');
-const tip = document.querySelector('.tip');
-let tipPercent = '';
-const custom = document.querySelector('.button-custom');
-const bill = document.querySelector('.bill');
-let billAmount = '';
-const number = document.querySelector('.number');
-let numberOfPeople = '';
-let tipAmount = document.querySelector('.tip-amount');
-let total = document.querySelector('.total');
-const reset = document.querySelector('.button-reset');
+const billInput = document.querySelector('.bill');
+const tipButtons = document.querySelectorAll('.button-tip');
+const customInput = document.querySelector('.button-custom');
+const numberInput = document.querySelector('.number');
 const errorBill = document.querySelector('.error-message-bill');
 const errorPeople = document.querySelector('.error-message-people');
+const resetButton = document.querySelector('.button-reset');
+let billAmount = '';
+let tipPercent = '';
+let numberOfPeople = '';
+let tipAmount = document.querySelector('.calculation-tip');
+let totalPerPerson = document.querySelector('.calculation-total');
 
-bill.addEventListener('input', (event) => {
+billInput.addEventListener('input', (event) => {
   billAmount = event.target.value;
+  billInput.classList.remove('error');
   errorBill.classList.remove('display')
-  bill.classList.remove('error');
   if (billAmount === '0') {
-    bill.classList.add('error');
+    billInput.classList.add('error');
     errorBill.classList.add('display');
   }
   calculateTip();
 })
 
-number.addEventListener('input', (event) => {
-  numberOfPeople = event.target.value;
-  number.classList.remove('error')
-  errorPeople.classList.remove('display');
-  if (numberOfPeople === '0') {
-    number.classList.add('error')
-    errorPeople.classList.add('display');
-  }
-  calculateTip();
+tipButtons.forEach((tipButton) => {
+  tipButton.addEventListener('click', (event) => {
+    tipPercent = event.target.value;
+    tipButtons.forEach((tipButton) => {
+      tipButton.classList.remove('active')
+    });
+    event.target.classList.add('active');
+    customInput.value = "";
+    calculateTip();
+  })
 })
 
-custom.addEventListener('input', (event) => {
-  buttons.forEach((button) => {
+customInput.addEventListener('input', (event) => {
+  tipButtons.forEach((button) => {
     button.classList.remove('active')
   });
   tipPercent = event.target.value/100;
   calculateTip();
 })
 
-buttons.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    tipPercent = event.target.value;
-    buttons.forEach((button) => {
-      button.classList.remove('active')
-    });
-    event.target.classList.add('active');
-    custom.value = "";
-    calculateTip();
-  })
+numberInput.addEventListener('input', (event) => {
+  numberOfPeople = event.target.value;
+  numberInput.classList.remove('error')
+  errorPeople.classList.remove('display');
+  if (numberOfPeople === '0') {
+    numberInput.classList.add('error')
+    errorPeople.classList.add('display');
+  }
+  calculateTip();
 })
 
 const calculateTip = () => {
   if ((numberOfPeople > 0) && (tipPercent > 0) && (billAmount > 0)) {
     tipAmount.innerText = `$${(billAmount * tipPercent).toFixed(2)}`;
-    total.innerText = `$${(billAmount * tipPercent / numberOfPeople).toFixed(2)}`;
-    reset.classList.add('active');
+    totalPerPerson.innerText = `$${(billAmount * tipPercent / numberOfPeople).toFixed(2)}`;
+    resetButton.classList.add('active');
   }
 }
 
-reset.addEventListener('click', (event) => {
+resetButton.addEventListener('click', (event) => {
   tipAmount.innerText = '$0.00'
-  total.innerText = '$0.00'
+  totalPerPerson.innerText = '$0.00'
   event.target.classList.remove('active');
-  buttons.forEach((button) => {
-    button.classList.remove('active')
+  tipButtons.forEach((tipButton) => {
+    tipButton.classList.remove('active')
   });
-  bill.value = "";
-  number.value = "";
-  custom.value = "";
+  billInput.value = "";
+  customInput.value = "";
+  numberInput.value = "";
 })
